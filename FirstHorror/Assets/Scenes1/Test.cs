8d210aa14439484906cfd892b9022e8e28cabfd4
40 <sallinekotan@gmail.com>
2023-06-11 17:41:14 +0200

    first commit
    3д кубики и тест скрипт

---
+++ FirstHorror/Assets/Scenes1/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called befo4q2879o364o87q2346eo8127364o8p127364o8127346p817236yo8eqiw7tyrdo8kqw7uertygqkweyurre the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
